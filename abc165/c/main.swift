c4a2bd7e77478aef48626cf8dd921ca082b06813
Shota Kashihara <mercury4th@gmail.com>
2020-06-29 02:25:02 +0900

    update

--- abc165/c/main.swift
+++ abc165/c/main.swift
@@ -1,4 +1,20 @@
+// let (n,m,q) = cin.triple
 
+// /// 全通りの長さn {1...m} の組み合わせを考えてみる
+// /// 1,2,3
+// /// 1,2,4
+// /// 2,3,4
+
+
+// for i in (1...q) {
+//     let (a,b,c,d) = cin.quadro
+
+
+// }
+
+
+// print([1,2,3,4,5,6].permutation(length: 3))
+print([1,2,3,4].combination(length: 3))
 
 import Foundation
 #if os(Linux)
@@ -14,6 +30,8 @@ enum cin {
     static var pair: (Int, Int) { let line = readLine()!.components(separatedBy: " "); return (Int(line[0])!, Int(line[1])!) }
     // 12 23 34
     static var triple: (Int, Int, Int) { let line = readLine()!.components(separatedBy: " "); return (Int(line[0])!, Int(line[1])!, Int(line[2])!) }
+    // 12 23 34 45
+    static var quadro: (Int, Int, Int, Int) { let line = readLine()!.components(separatedBy: " "); return (Int(line[0])!, Int(line[1])!, Int(line[2])!, Int(line[3])!) }
     // AtCoder
     static var str: String { readLine()! }
     // At Coder
@@ -26,6 +44,9 @@ enum cin {
     static func v_array(count: Int) -> [Int] { (0..<count).map { _ in int } }
 }
 
+/// ヘルパー関数ライブラリ
+/// https://github.com/ankurp/Dollar
+/// https://github.com/ankurp/Cent
 extension Int {
     // 123 -> [1, 2, 3]
     var digits: [Int] { Array(String(self)).map { Int(String($0))! } }
@@ -65,5 +86,103 @@ extension String {
         var characters = Array(self)
         characters.swapAt(index1, index2)
         self = String(characters)
+    }
+}
+
+extension Array {
+    //  ExSwift
+    //
+    //  Created by pNre on 03/06/14.
+    //  Copyright (c) 2014 pNre. All rights reserved.
+    //
+    //  https://github.com/pNre/ExSwift/blob/master/ExSwift/Array.swift
+    //  https://github.com/pNre/ExSwift/blob/master/LICENSE
+    /**
+        - parameter length: The length of each permutation
+        - returns: All permutations of a given length within an array
+    */
+    func permutation (length: Int) -> [[Element]] {
+        if length < 0 || length > self.count {
+            return []
+        } else if length == 0 {
+            return [[]]
+        } else {
+            var permutations: [[Element]] = []
+            let combinations = combination(length: length)
+            for combination in combinations {
+                var endArray: [[Element]] = []
+                var mutableCombination = combination
+                permutations += self.permutationHelper(n: length, array: &mutableCombination, endArray: &endArray)
+            }
+            return permutations
+        }
+    }
+    private func permutationHelper(n: Int, array: inout [Element], endArray: inout [[Element]]) -> [[Element]] {
+        if n == 1 {
+            endArray += [array]
+        }
+        for i in (0..<n) {
+            permutationHelper(n: n - 1, array: &array, endArray: &endArray)
+            let j = n % 2 == 0 ? i : 0
+            let temp: Element = array[j]
+            array[j] = array[n - 1]
+            array[n - 1] = temp 
+        }
+        return endArray
+    }
+    /**
+        - parameter length:
+        - returns: Returns all of the combinations in the array of the given length
+    */
+    func combination (length: Int) -> [[Element]] {
+        if length < 0 || length > self.count {
+            return []
+        }
+        var indexes: [Int] = (0..<length).reduce([]) {$0 + [$1]}
+        var combinations: [[Element]] = []
+        let offset = self.count - indexes.count
+        while true {
+            var combination: [Element] = []
+            for index in indexes {
+                combination.append(self[index])
+            }
+            combinations.append(combination)
+            var i = indexes.count - 1
+            while i >= 0 && indexes[i] == i + offset {
+                i -= 1
+            }
+            if i < 0 {
+                break
+            }
+            i += 1
+            let start = indexes[i-1] + 1
+            for j in (i-1)..<indexes.count {
+                indexes[j] = start + j - i + 1
+            }
+        }
+        return combinations
+    }
+    /**
+        - parameter length: The length of each permutations
+        - returns: All of the permutations of this array of a given length, allowing repeats
+    */
+    func repeatedPermutation(length: Int) -> [[Element]] {
+        if length < 1 {
+            return []
+        }
+        var indexes: [[Int]] = []
+        indexes.repeatedPermutationHelper(seed: [], length: length, arrayLength: self.count, indexes: &indexes)
+        return indexes.map({ $0.map({ i in self[i] }) })
+    }
+    private func repeatedPermutationHelper(seed: [Int], length: Int, arrayLength: Int, indexes: inout [[Int]]) {
+        if seed.count == length {
+            indexes.append(seed)
+            return
+        }
+        for i in (0..<arrayLength) {
+            var newSeed: [Int] = seed
+            newSeed.append(i)
+            self.repeatedPermutationHelper(seed: newSeed, length: length, arrayLength: arrayLength, indexes: &indexes)
+        }
     }
 }
